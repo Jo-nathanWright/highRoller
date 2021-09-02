3853671bf52bcfea85bf507c7e89cdb93677e820
ightTale <j.wright72127@gmail.com>
2021-09-02 10:44:57 -0600

    Text Changes

--- Program.cs
+++ Program.cs
@@ -8,36 +8,70 @@ namespace highRoller
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Hi High Roller! Want to Roll the dice? Press Anykey to Continue, q to quit.");
-      Console.Write("-");
+            string intro = "Hi High Roller! Want to Roll the dice? Press Anykey to Continue, q to quit.";
+            foreach (char c  in intro)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
       char response = Console.ReadKey().KeyChar;
       Console.Clear();
       bool rolling = true;
-      while(rolling){
           if(response == 'q'){
-          Console.WriteLine("Leaving So Soon?");
+          string quit = "Leaving So Soon?";
+          foreach (char c  in quit)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
           rolling = false;
         } else {
-            Console.WriteLine(@"First we need to know the dice your playing with.
+            string info = @"First we need to know the dice your playing with.
     - How many Are You playing with?
     - How many sides do they have?
-    - Whats your Dice Modifier?
-    ");
-      Console.Write("Total Dice: ");
+    - Whats your Dice Modifier?";
+            foreach (char c  in info)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+            Console.WriteLine("\n");
+        while(rolling){
+      string TotalDice = "Total Dice: ";
+            foreach (char c  in TotalDice)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
       string totalString = Console.ReadLine();
       int total = Int32.Parse(totalString);
 
-      Console.Write("Dice Sides: ");
-      string sidesString = Console.ReadLine();
+          string DiceSides = "Dice Sides: ";
+          foreach (char c  in DiceSides)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+          string sidesString = Console.ReadLine();
       int sides = Int32.Parse(sidesString);
 
-      Console.Write("Total Modifier: ");
+      string TotalMod =  "Total Modifier: ";
+      foreach (char c  in TotalMod)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
       string modString = Console.ReadLine();
       int mod = Int32.Parse(modString);
       Console.WriteLine("\n");
 
-          Console.Write("Rolling Dice");
-          string dots = "........";
+          string rollingDice = "Rolling Dice";
+          foreach (char c  in rollingDice)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+          string dots = ".....";
           foreach (char c  in dots)
           {
             Console.Write(c);
@@ -50,29 +84,56 @@ namespace highRoller
       for (int i = 0; i < total; i++)
       {
         int roll = Rand.Next(sides) + 1;
-        Console.WriteLine("You rolled a " + roll);
-        totalRoll += roll;
-      }
-      Console.WriteLine("\n");
-      Console.WriteLine("This is the Total Roll: " + (totalRoll + mod));
+            totalRoll += roll;
+          }
+          Console.WriteLine("\n");
+          int finalTotal = totalRoll + mod;
+          string finalResult = "This is the Total Roll: " + (finalTotal);
+      foreach (char c  in finalResult)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+          Console.WriteLine("\n");
 
         bool leaving = true;
         while (leaving){
-            Console.WriteLine("Want To Roll Again? y/n");
-            Console.Write("-");
+            string question = "Want To Roll Again? y/n";
+            foreach (char c  in question)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+            Console.WriteLine("");
             char answer = Console.ReadKey().KeyChar;
             Console.WriteLine("\n");
             if(answer == 'n'){
-                Console.WriteLine("Thanks For Rolling With Us.");
+                string n = "Thanks For Rolling With Us.";
+                foreach (char c  in n)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
                 rolling = false;
                 leaving = false;
             } else if(answer == 'y') {
-                Console.WriteLine("Lets Roll Again Partner!");
+                string y = "Lets Roll Again Partner!";
+                foreach (char c  in y)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
                 Console.Clear();
                 leaving = false;
             } else {
-                Console.WriteLine("Please Select A correct Key.");
-                Console.Beep();
+                string tryAgain = "Please Select A correct Key.";
+                foreach (char c  in tryAgain)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+              Console.WriteLine("\n");
+              Console.Beep();
           }
         }
         }
